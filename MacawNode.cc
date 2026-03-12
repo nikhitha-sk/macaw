2bcdc0ec762f25b8aa7f4ce3013907a9016748cd
Srujan M Padmashali <srujanmpadmashali@gmail.com>
2026-03-12 07:05:13 +0530

    second

--- MacawNode.cc
+++ MacawNode.cc
@@ -1,11 +1,19 @@
+
 #include "MacawNode.h"
 
 Define_Module(MacawNode);
 
 void MacawNode::initialize()
 {
-    if (getIndex() == 0) {
-        scheduleAt(simTime() + 1, new cMessage("RTS"));
+    if (getIndex() == sender) {
+        // Find which out[] index leads to receiver
+        int outIndex = (receiver > sender) ? receiver - 1 : receiver;
+        cMessage *rts = new cMessage("RTS");
+        scheduleAt(simTime() + 1, rts);
+        // Send RTS to receiver after 1s
+        cMessage *rtsNet = new cMessage("RTS");
+        sendDelayed(rtsNet, 1.0, "out", outIndex);
+        state = WAIT_CTS;
     }
 }
 
@@ -13,32 +21,62 @@ void MacawNode::handleMessage(cMessage *msg)
 {
     EV << "Node " << getIndex() << " received " << msg->getName() << endl;
 
-    if (strcmp(msg->getName(), "RTS") == 0) {
-        bubble("RTS received");
+    int n = gateSize("out");
 
-        cMessage *cts = new cMessage("CTS");
-        send(cts, "out");
+    if (strcmp(msg->getName(), "RTS") == 0) {
+        if (getIndex() == receiver && state == IDLE) {
+            bubble("RTS received, sending CTS");
+            cMessage *cts = new cMessage("CTS");
+            send(cts, "out", sender); // send CTS to sender only
+            state = WAIT_DS;
+        } else if (getIndex() != sender && getIndex() != receiver) {
+            bubble("Neighbor heard RTS, backing off");
+            state = BUSY;
+        }
     }
     else if (strcmp(msg->getName(), "CTS") == 0) {
-        bubble("CTS received");
-
-        cMessage *ds = new cMessage("DS");
-        send(ds, "out");
+        if (getIndex() == sender && state == WAIT_CTS) {
+            bubble("CTS received, sending DS");
+            // DS is broadcast to both neighbors (out[0] and out[1])
+            for (int i = 0; i < n; ++i) {
+                cMessage *ds = new cMessage("DS");
+                send(ds, "out", i);
+            }
+            state = WAIT_ACK;
+        } else if (getIndex() != sender && getIndex() != receiver) {
+            bubble("Neighbor heard CTS, staying quiet");
+            state = BUSY;
+        }
     }
     else if (strcmp(msg->getName(), "DS") == 0) {
-        bubble("DS received");
-
-        cMessage *data = new cMessage("DATA");
-        send(data, "out");
+        if (getIndex() == sender) {
+            bubble("Sender heard DS, sending DATA");
+            // Find which out[] index leads to receiver
+            int outIndex = (receiver > sender) ? receiver - 1 : receiver;
+            cMessage *data = new cMessage("DATA");
+            send(data, "out", outIndex);
+        } else if (getIndex() == receiver) {
+            bubble("Receiver heard DS, waiting for DATA");
+        } else {
+            bubble("Neighbor heard DS, staying quiet");
+            state = BUSY;
+        }
     }
     else if (strcmp(msg->getName(), "DATA") == 0) {
-        bubble("DATA received");
-
-        cMessage *ack = new cMessage("ACK");
-        send(ack, "out");
+        if (getIndex() == receiver && state == WAIT_DS) {
+            bubble("DATA received, sending ACK");
+            // Find which out[] index leads to sender
+            int outIndex = (sender > receiver) ? sender - 1 : sender;
+            cMessage *ack = new cMessage("ACK");
+            send(ack, "out", outIndex);
+            state = IDLE;
+        }
     }
     else if (strcmp(msg->getName(), "ACK") == 0) {
-        bubble("ACK received");
+        if (getIndex() == sender && state == WAIT_ACK) {
+            bubble("ACK received, transmission complete");
+            state = IDLE;
+        }
     }
 
     delete msg;
--- MacawNode.h
+++ MacawNode.h
@@ -5,9 +5,14 @@
 
 using namespace omnetpp;
 
+enum State {IDLE, WAIT_CTS, WAIT_DS, WAIT_ACK, BUSY};
+
 class MacawNode : public cSimpleModule
 {
   protected:
+    State state = IDLE;
+    int sender = 0;
+    int receiver = 1;
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
 };
